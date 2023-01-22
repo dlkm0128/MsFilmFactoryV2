226feb59d3118eda8624c9902849d5e2653648fa
mrmalikoffl <121479735+mrmalikoffl@users.noreply.github.com>
2023-01-22 14:19:56 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -384,7 +384,7 @@ async def get_shortlink(link):
         link = link.replace("http", https)
 
     url = f'https://tnlink.in/api'
-    params = {'token': URL_SHORTNER_WEBSITE_API,
+    params = {'api': URL_SHORTNER_WEBSITE_API,
               'link': link,
               'format': 'json'
               }
@@ -397,8 +397,8 @@ async def get_shortlink(link):
                     return data['shortlink']
                 else:
                     logger.error(f"Error: {data['message']}")
-                    return f'https://tnlink.in/api?token={URL_SHORTNER_WEBSITE_API}&link={link}'
+                    return f'https://tnlink.in/st?api={URL_SHORTNER_WEBSITE_API}&link={link}'
 
     except Exception as e:
         logger.error(e)
-        return f'https://tnlink.in/api?token={URL_SHORTNER_WEBSITE_API}&link={link}'
+        return f'https://tnlink.in/st?api={URL_SHORTNER_WEBSITE_API}&link={link}'
